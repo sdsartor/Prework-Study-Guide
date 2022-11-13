bcdf717b850bfd5a510263c9838cde4f0ae0e9c8
sdsartor <sdsartor48@gmail.com>
2022-11-13 12:22:55 -0800

    Titel enhancement

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,17 @@ function listTopics() {
 }
 
 function selectTopic() {
-if (randomTopic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (randomTopic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (randomTopic === 'Git') {
- console.log("Let's study Git!");
-} else if (randomTopic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}      
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
 
 console.log('Here are the topics we learned through Prework:');
